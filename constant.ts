b66f20a211132d9fcfd88d3a6573524188acac79
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2023-12-22 10:13:15 -0500

    agregando lista de archivos

---
+++ constant.ts
@@ -0,0 +1,10 @@
+export const COLOR_EXTENSION_MAP: {
+    [key: string]: string;
+} = {
+    pdf: "#0160FE",
+    docs: "#2b579a",
+    doc: "#2b579a",
+    jpg: "#d4af37",
+    jpeg: "#d4af37",
+    txt: "#000000"
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,25 +3,39 @@ import { auth } from "@clerk/nextjs";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../firebase";
 import { FileType } from "../../../typings";
+import TableWrapper from "@/components/table/TableWrapper";
 
 async function Dashboard() {
   const { userId } = auth()
 
-  // const docResults = await getDocs(collection(db, "users", userId!, "files"))
+  const docResults = await getDocs(collection(db, "users", userId!, "files"))
   
-  // const skeletonFiles: FileType[] =  docResults.docs.map((doc) => ({
-  //   id: doc.id,
-  //   filename: doc.data().filename || doc.id,
-  //   timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
-  //   fullname: doc.data().fullname,
-  //   downloadURL: doc.data().downloadURL,
-  //   type: doc.data().type,
-  //   size: doc.data().size
-  // }))   
+  const skeletonFiles: FileType[] =  docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullname: doc.data().fullname,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size
+  }))   
+  console.log(skeletonFiles)
 
   return (
-    <div>
+    <div className="border-t">
       <DropzoneComponent />
+
+      <section className="container space-y-5">
+        <h2 className="font-bold">All files</h2>
+
+        <div>
+        <TableWrapper 
+          skeletonFiles={skeletonFiles}
+        />
+        </div>
+
+      </section>
+
     </div>
   )
 }
